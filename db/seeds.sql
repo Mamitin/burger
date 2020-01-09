4dedca88b751f135200497ef5f0df463bc1ffdd5
Monica Amitin <monzielvsu@gmail.com>
2020-01-09 14:13:43 -0800

    added folders and files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 -- Three entries to populate table
-INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger with avocado, false");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Blackbean burger with onions, false");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom burger with swiss cheese, false");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger with avocado", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blackbean burger with onions", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom burger with swiss cheese", false);
